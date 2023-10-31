b566a6b4332cf91bae5e868365663472187dceec
Maxim Pokrovsky <maxim.pokrovsky1807@gmail.com>
2023-10-31 11:01:03 +0300

    Create abstract class CreatedAt

--- blogicum/core/models.py
+++ blogicum/core/models.py
@@ -1,14 +1,9 @@
 from django.db import models
 
 
-class PublishedCreatedModel(models.Model):
-    """Absract model. Add creation time and flag is_published."""
+class CreatedAt(models.Model):
+    """Abstract model. Add creation time."""
 
-    is_published = models.BooleanField(
-        default=True,
-        verbose_name='Опубликовано',
-        help_text='Снимите галочку, чтобы скрыть публикацию.'
-    )
     created_at = models.DateTimeField(
         auto_now_add=True,
         verbose_name='Добавлено',
@@ -17,3 +12,16 @@ class PublishedCreatedModel(models.Model):
     class Meta:
         abstract = True
         ordering = ('-created_at',)
+
+
+class PublishedCreatedModel(CreatedAt):
+    """Absract model. Add creation time and flag is_published."""
+
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано',
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
+    )
+
+    class Meta(CreatedAt.Meta):
+        abstract = True
